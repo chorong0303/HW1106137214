040743b424fa5ddb6ae122cfdb37eddf98f17d05
chorong0303 <1106137214@nkust.edu.tw>
2021-05-30 00:26:06 +0800

    add checkbox

--- 0527/App.js
+++ 0527/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControllLabel from '@material-ui/core/FormControlLabel';
+function CheckboxExample(){
+  const [checked, setchecked] = React.useState(true) 
+    return(
+      <FormControllLabel  control = {<Checkbox checked = {checked} onChange = {(e) => setchecked(e.target.checked)} inputProps  = {{'aria-label' : 'primary checkbox'}} icon = {<SaveIcon />} checkedIcon ={<DeleteIcon />} />} label = "Checkbox1" />
+  
+        
+    )
+  }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <CheckboxExample />
       <ButtonGroup variant = "contained" size = "large" >
         <Button  color = "secondary" startIcon = {<SaveIcon />}>
           Save
@@ -16,6 +28,7 @@ function App() {
           Delete
         </Button>
         </ButtonGroup>
+        
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
